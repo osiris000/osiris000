47d3269fb0cc7b1c38f1f545a599ee1d914965dd
root <root@osiris.osiris.net>
2023-07-19 06:13:01 +0200

    upload2

---
+++ html/lib/js/hxr.js
@@ -0,0 +1,154 @@
+/*  
+
+funciones para tratar con el objeto xhr-ajax
+
+*/
+
+
+function sEval(obj) {
+return eval(obj)
+//    return Function('"use strict";return (' + obj + ')')();
+}
+
+
+$ajax = new Array()
+
+
+function ajax(datas,ih,loc,method="GET",js,mode="=",masajax=$ajax.length){
+
+mode = mode
+
+$ajax[masajax] =  new XMLHttpRequest()
+
+
+if(method.toUpperCase()=="GET"){
+loc = loc+"?"+datas;    
+}
+
+$ajax[masajax].open(method,loc)
+
+
+$ajax[masajax].onreadystatechange = function(){
+
+
+
+
+
+if($ajax[masajax].readyState===4){
+
+if(ih){
+
+sEval(`document.getElementById('`+ih+`').innerHTML` + eval(`mode`) + `$ajax[`+masajax+`].responseText`)
+
+
+} if(js) sEval(js) ; $ajax[masajax] = "END 4:"+masajax } else {
+
+	if($ajax[masajax].readyState==3){
+	if(!ih) sEval($ajax[masajax].responseText)
+		}
+        //readyState < 4
+	}
+//end function
+}
+
+
+
+if(method.toUpperCase()=="POST"){
+	
+	$ajax[masajax].setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+	$ajax[masajax].send(datas)
+	
+} else if(method.toUpperCase()==="GET"){
+	$ajax[masajax].send()
+}
+
+}
+
+
+
+
+
+
+/* otras funciones a depurar*/
+
+
+function getId(thid){return encodeURIComponent(document.getElementById(thid).value);}
+function EUwysiwyg(thid){return encodeURIComponent(document.getElementById(thid).contentDocument.body.innerHTML);}
+function gId(thid){return document.getElementById(thid);}
+function GId(thid){return document.getElementById(thid).value;}
+
+
+
+function dmenu(divid,classname){
+    var dcls=document.getElementsByClassName(classname);
+    for(i=0;i<dcls.length;i++){
+    if(dcls[i].id!==divid){
+    dcls[i].style.visibility="hidden";
+    dcls[i].style.display="none";
+    }else{
+    dcls[i].style.visibility="visible";
+    dcls[i].style.display="block";
+     }
+   }
+ }
+ 
+
+
+function display(id){
+if(!id) return "Undefined";
+id = document.getElementById(id)
+dsplay = id.style.display
+if(dsplay=='block') id.style.display = "none"
+else id.style.display = "block"
+return id.style.display
+}
+
+
+
+function clearHTML(id){
+if(!id) return "Undefined";
+id = document.getElementById(id)
+id.innerHTML = "";
+}
+
+
+ function emulaClick(emulado){
+var y = document.querySelector(emulado);
+y.click();
+return;
+}
+
+
+ function clickId(emulado){
+var y = document.querySelector(emulado);
+y.click();
+return;
+}
+
+
+
+function xplayer(v,src,type){
+
+  var video = document.getElementById(v);
+  var videoSrc = src;
+  //
+  // First check for native browser HLS support
+  //
+  if (video.canPlayType(type)) {
+    video.src = videoSrc;
+    video.play = true
+    //
+    // If no native HLS support, check if HLS.js is supported
+    //
+  } else if (Hls.isSupported()) {
+    var hls = new Hls();
+    hls.loadSource(videoSrc);
+    hls.attachMedia(video);
+  }
+
+
+document.getElementById("mg").innerHTML += "<br>"+v+"<br>"+src+"<br>"+type+"<br>"
+
+
+
+}
