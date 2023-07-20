5c9ada80b99b62ade418ce23d306b6149091534f
root <root@osiris.osiris.net>
2023-07-20 08:19:48 +0200

    upload push

--- osiris.sh
+++ osiris.sh
@@ -6,18 +6,39 @@
 #python3
 
 check_command_installed() {
+
   if command -v "$1" >/dev/null 2>&1; then
+
     version_output="$($1 -V 2>&1)"
-    echo "$1 está instalado en el sistema. Versión: $version_output"
-  else
     cat <<EOF
-$1 no está instalado en el sistema
-Pruebe "apt install $1" (o el comando adecuado para su sistema)
+
+     *********************************************
+         $1 está instalado en el sistema.
+     ---------------------------------------------
+      Versión: $version_output 
+     *********************************************
+     
 EOF
+
+  else
+
+    cat <<EOF
+
+----------------------------------------------------------------    
+   $1 no está instalado en el sistema
+Pruebe "apt install $1" (o el comando adecuado para su sistema)
+----------------------------------------------------------------
+
+EOF
+	exit
   fi
 }
 
-
 check_command_installed python3
 
+check_command_installed /usr/sbin/apache2
+
+check_command_installed /usr/bin/php
+
 check_command_installed ffmpeg
+
