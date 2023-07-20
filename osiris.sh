f73fa71fb21cf0168983b674ca377562fa946817
root <root@osiris.osiris.net>
2023-07-20 08:30:23 +0200

    upload push

--- osiris.sh
+++ osiris.sh
@@ -1,44 +1,33 @@
 #!/bin/sh
 
+# Comprobación de requisitos mínimos para primera instalación.
 
-#comprobación de requisitos mínimos para primera instalación, 
-
-#python3
-
+# Función para comprobar si un comando está instalado.
 check_command_installed() {
-
   if command -v "$1" >/dev/null 2>&1; then
-
     version_output="$($1 -V 2>&1)"
     cat <<EOF
-
-     *********************************************
-         $1 está instalado en el sistema.
-     ---------------------------------------------
-      Versión: $version_output 
-     *********************************************
-     
+*************************************************
+  $1 está instalado en el sistema.
+----------------------------------------------
+  Versión: $version_output
+*************************************************
 EOF
-
   else
-
     cat <<EOF
-
-----------------------------------------------------------------    
-   $1 no está instalado en el sistema
-Pruebe "apt install $1" (o el comando adecuado para su sistema)
 ----------------------------------------------------------------
-
+  $1 no está instalado en el sistema.
+  Pruebe "apt install $1" (o el comando adecuado para su sistema)
+----------------------------------------------------------------
 EOF
-	exit
+    exit
   fi
 }
 
+# Comprobación de comandos instalados.
 check_command_installed python3
-
 check_command_installed /usr/sbin/apache2
-
 check_command_installed /usr/bin/php
-
 check_command_installed ffmpeg
+
 
