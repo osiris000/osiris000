741dba4426a169416c3a8653bd055a8005bd663e
root <root@osiris.osiris.net>
2023-07-20 09:24:00 +0200

    osiris sh

--- osiris.sh
+++ osiris.sh
@@ -2,37 +2,51 @@
 
 # Comprobación de requisitos mínimos para primera instalación.
 
-# Función para comprobar si un comando está instalado.
-check_command_installed() {
-  if command -v "$1" >/dev/null 2>&1; then
-    version_output="$($1 -V 2>&1)"
-    command_name=$(basename "$1")
-    cat <<EOF
-**********************************************************
-  $command_name está instalado en el sistema.
 
-  Versión: $version_output
-  
-***********************************************************  
-***********************************************************
-EOF
+check_command_installed() {
+  command_path=$1
+  version_option=$2
+
+  # Verificar con la ruta del ejecutable
+  if [ -x "$command_path" ]; then
+    version_output="$($command_path $version_option </dev/null 2>&1)"
+    command_name=$(basename "$command_path")
+    echo "******************************************************************"
+    printf "%-64s%s\n" " $command_name está instalado en la ruta especificada." "*"
+    echo "------------------------------------------------------------------"
+    echo "$version_output"
+    echo "******************************************************************"
   else
-    command_name=$(basename "$1")
-    cat <<EOF
---------------------------------------------------------------------------------
-  $command_name no está instalado en el sistema.
-  Pruebe "apt install $command_name" (o el comando adecuado para su sistema)
---------------------------------------------------------------------------------
-EOF
-    exit
+    command_name=$(basename "$command_path")
+    echo "------------------------------------------------------------------"
+    printf "%-69s%s\n" " $command_name no está instalado en la ruta especificada."
+    echo " Se está verificando con dpkg para obtener más información."
+    echo "------------------------------------------------------------------"
+    
+    # Verificar con el sistema de gestión de paquetes
+    dpkg_output=$(dpkg -l "$command_name" 2>&1)
+    if [ $? -eq 0 ]; then
+      echo "******************************************************************"
+      printf "%-64s%s\n" " $command_name está instalado en el sistema (según dpkg)." "*"
+      echo "------------------------------------------------------------------"
+      echo "$dpkg_output" | tail -n +6
+      echo "******************************************************************"
+    else
+      echo "------------------------------------------------------------------"
+      printf "%-69s%s\n" " $command_name no está instalado en el sistema (según dpkg) tampoco."
+      echo " Pruebe \"apt install $command_name\" (o el comando adecuado para su sistema)"
+      echo "------------------------------------------------------------------"
+      exit 1
+    fi
   fi
 }
 
+
 # Comprobación de comandos instalados.
-check_command_installed /usr/bin/python3
-check_command_installed /usr/sbin/apache2
-check_command_installed /usr/bin/php
-check_command_installed /usr/bin/ffmpeg
+check_command_installed /usr/bin/python3 -V
+check_command_installed /usr/sbin/apache2 -v
+check_command_installed /usr/bin/php -v
+check_command_installed /usr/bin/ffmpeg -v
 
 
 
