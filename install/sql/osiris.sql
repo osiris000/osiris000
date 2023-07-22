e6c5b03e3a4369e278b023943b2b53e89f38b7bb
root <root@osiris.osiris.net>
2023-07-22 06:19:51 +0200

    web sql

--- install/sql/osiris.sql
+++ install/sql/osiris.sql
@@ -1,8 +1,8 @@
 -- Crear la base de datos "osiris"
-CREATE DATABASE IF NOT EXISTS osiris;
+CREATE DATABASE IF NOT EXISTS osiris_web;
 
 -- Usar la base de datos "osiris"
-USE osiris;
+USE osiris_web;
 
 -- Enlazar el archivo "tablas.sql" al final
 SOURCE tablas.sql;
--- install/sql/tablas.sql
+++ install/sql/tablas.sql
@@ -1,1 +1,13 @@
 #SQL Tables
+
+
+#Tabla para usuarios 
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user VARCHAR(50) NOT NULL,
+  password VARCHAR(128) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  telefono VARCHAR(20) NOT NULL,
+  fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
