c9f771ab648ed6467d4817be76a65c939ced75e0
root <root@osiris.osiris.net>
2023-07-22 04:13:30 +0200

    osiris-sql

---
+++ install/sql/osiris.sql
@@ -0,0 +1,8 @@
+-- Crear la base de datos "osiris"
+CREATE DATABASE IF NOT EXISTS osiris;
+
+-- Usar la base de datos "osiris"
+USE osiris;
+
+-- Enlazar el archivo "tablas.sql" al final
+SOURCE tablas.sql;
