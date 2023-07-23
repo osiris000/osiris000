d7c810e726c87bc32004703fc137ba78843feb0a
root <root@osiris.osiris.net>
2023-07-23 04:49:41 +0200

    install webapp ...

--- bin/com/install.py
+++ bin/com/install.py
@@ -1,8 +1,27 @@
-#comprobar futuro que osiris.py no haya sido cargado para ejecución directa
-
-
-
-
 def main(args):
-	print("Args dentro de x.py:", args)
-	print("install.py init")
+    print("Args dentro de install.py:", args)
+    
+    if args:
+        parse(args)
+    else:
+        print("No se proporcionaron argumentos.")
+
+def parse(args):
+    # Aquí puedes simular cómo se procesarían los argumentos para cada comando
+    if args[0] == "auto-install":
+        auto_install_args = args[1:]  # Argumentos para el comando auto-install
+        print("Simulando ejecución de auto-install con argumentos:", auto_install_args)
+        # Llamar a la función correspondiente para el comando auto-install con los argumentos
+        # Ejemplo: auto_install_function(auto_install_args)
+    elif args[0] == "webapp":
+        webapp_args = args[1:]  # Argumentos para el comando webapp
+        print("Simulando ejecución de webapp con argumentos:", webapp_args)
+        # Llamar a la función correspondiente para el comando webapp con los argumentos
+        # Ejemplo: webapp_function(webapp_args)
+    else:
+        print("Comando no reconocido para install.py:", args[0])
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv[1:])  # Pasamos los argumentos sin el primer elemento que es el nombre del script
+
