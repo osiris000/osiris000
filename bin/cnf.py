7db34e5dc45b078e19735767b37a46d7be59fd11
root <root@osiris.osiris.net>
2023-07-23 16:38:54 +0200

    com

--- bin/cnf.py
+++ bin/cnf.py
@@ -3,7 +3,11 @@ PATH_DAT = "./data"
 
 PATH = {
 
-"PATH_HELP" : "./help"
+"PATH_HELP" : "./help",
+
+"HELP" : "./help",
+
+"DATA" : "./data"
 
 }
 
@@ -13,5 +17,7 @@ FileAuth = PATH_DAT+"/auth_pwd"
 Reset_Password_Key = "SECRET_RECOVER"
 
 
+# archivo historial de comandos
+history_file = PATH["DATA"] + "/" + "command_history.pkl"
 # longitud historial de comandos
 history_com_size = 100
