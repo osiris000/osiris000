89be81fe3688cc0d8ba5f682c5eae2ac2ea91d83
root <root@osiris.osiris.net>
2023-10-05 08:13:09 +0200

    bin-com

--- bin/com/sniff.py
+++ bin/com/sniff.py
@@ -2,20 +2,33 @@ from scapy.all import *
 
 print('Creado módulo-comando sniff y fecha y hora: 2023-10-02 12:00:26.017185')
 
-
 def main(args):
     print('Args dentro de sniff', args)
 
-
-    if len(args)<1:
-        print("Requiere como argumento una interface de red")
+    # Verificar si hay argumentos
+    if len(args) < 1:
+        print("Requiere un argumento ")
+        # se sale
         return
 
+    if args[0] == "--interfaces":
+        # Obtener interfaces de red disponibles
+        interfaces = ifaces
+        if not interfaces:
+            print("No hay interfaces de red disponibles")
+            return
 
-    iface = args[0]  #  nombre de interfaz de red
+        print("Interfaces disponibles:")
+        for iface, details in interfaces.items():
+            print(f"Nombre: {iface}, Descripción: {details.description}")
+        return
+
+    iface = args[0]  # nombre de tu interfaz de red
     print(f"Monitoreando la red en la interfaz {iface}...\n")
-    sniff(iface=iface, prn=packet_handler);
-
+    try:
+        sniff(iface=iface, prn=packet_handler)
+    except Exception as e:
+        print("Error:",e)
 
 def packet_handler(packet):
     if packet.haslayer(IP):
@@ -31,5 +44,5 @@ def packet_handler(packet):
 
             print("\n" + "=" * 50 + "\n")
 
-#if __name__ == "__main__":
-#    main()
+if __name__ == "__main__":
+    main(sys.argv[1:])
