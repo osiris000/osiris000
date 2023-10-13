2f9dc84e6aa5ed964c23d076483d95dbf0e5f190
root <root@osiris.osiris.net>
2023-10-13 03:31:29 +0200

    bard

--- bin/com/bard.py
+++ bin/com/bard.py
@@ -3,10 +3,14 @@ from bardapi import Bard
 import requests
 import json
 import hashlib
+import subprocess
 
 os.environ['_BARD_API_KEY'] = ''
 
+
+
 token = os.environ['_BARD_API_KEY']
+
 
 
 bard_start = False
@@ -47,13 +51,51 @@ def bard_start_ini(token):
 
 
 
+
+def create_dir(path):
+    """
+    Crea un directorio si no existe.
+
+    Args:
+        path: La ruta del directorio a crear.
+
+    Returns:
+        True si el directorio se creó correctamente, False si el directorio ya existía.
+    """
+
+    try:
+        os.makedirs(path)
+        return True
+    except OSError as e:
+        if e.errno == errno.EEXIST and os.path.isdir(path):
+            return True
+        else:
+            raise
+
+
+
+
+dir_edit = "com/datas/bard"
+
+
+try:
+    create_dir(dir_edit)
+except Exception as e:
+    print("Error",e)
+
+
+
 archivo = "bard_2.log"
 limite = 8 * 512
 logread = "START_LOG\n"
 
 
-Previous = ""
-pie = ""
+Previous = """
+"""
+
+
+Footer = """
+"""
 
 
 if not os.path.exists(archivo):
@@ -84,11 +126,18 @@ def main(args):
 
 
 
+    if args[0] == "--edit":
+        try:
+            filename = args[1]
+            subprocess.call(["nano", "-w","-i",dir_edit +"/"+ filename])
+            return
+        except Exception as e:
+            print("Error:", e)
+            return
 
-    if args[0] == "--find-cookie":
-    	cookie_value = find_cookie_value(cookie_key,"google.com")
-    	print("Cookie VAlue:",cookie_value)
-    	return
+
+
+
 
     if args[0] == "--clear-log":
     	with open(archivo, "w") as f:
@@ -104,7 +153,7 @@ def main(args):
             return
         elif len(args) == 2:
             try:
-                with open(args[1], "r") as f:
+                with open(dir_edit+"/"+args[1], "r") as f:
                     global Previous
                     Previous = f.read().rstrip()
                     print("leido y cargado",args[1])
@@ -126,6 +175,38 @@ def main(args):
 
 
 
+    if args[0] == "--load-footer":
+
+        if len(args) < 2:
+            print("Faltan argumentos")
+            return
+        elif len(args) == 2:
+            try:
+                with open(dir_edit+"/"+args[1], "r") as f:
+                    global Footer
+                    Footer = f.read().rstrip()
+                    print("leido y cargado",args[1])
+                    return
+            except FileNotFoundError:
+                print("\nError:\n",FileNotFoundError)
+                return
+        return
+
+    if args[0] == "--clear-footer":
+        Footer = ""
+        print("Footer borrado")
+        return
+
+    if args[0] == "--show-footer":
+        print("Contenido de Footer:\n",Footer)
+        return
+
+
+
+
+
+
+
 
     recortar_archivo(archivo, limite)
 
@@ -138,9 +219,21 @@ def main(args):
 
 
     cuestion = " ".join(args)
-    cuestion =  Previous  + cuestion + pie
+    cuestion =  Previous  + cuestion + Footer
     print("\n*****************\nBard thinking")
-    answer = bard . get_answer ( cuestion)[ 'content' ]
+
+    #answer = bard . get_answer ( cuestion)
+    #print("RESPONSE:",answer)
+    #return
+
+    
+    try:
+        answer = bard . get_answer(cuestion)["content"]
+    except Exception as e:
+        print("Error:",e)
+        print("Compruebe que su key no ha cambiado")
+        return
+    
     print("\n Bard say....\n*************************************\n")
 
   # Recojo el texto a escribir en el archivo.
