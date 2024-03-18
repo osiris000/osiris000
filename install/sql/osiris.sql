34280f12cc0f8c7e1ed1656f8079560c1085fb62
root <root@osiris.osiris.net>
2024-03-18 07:04:34 +0100

    UFL 2024-03-18_07-04-34

--- install/sql/osiris.sql
+++ install/sql/osiris.sql
@@ -4,5 +4,24 @@ CREATE DATABASE IF NOT EXISTS osiris_web;
 -- Usar la base de datos "osiris"
 USE osiris_web;
 
--- Enlazar el archivo "tablas.sql" al final
-SOURCE tablas.sql;
+
+
+
+#Tabla para usuarios 
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  usr VARCHAR(50) NOT NULL,
+  pswd VARCHAR(128) NOT NULL,
+  email VARCHAR(100) NOT NULL UNIQUE,
+  telefono VARCHAR(20) DEFAULT '000000000',
+  fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  perm INT(4) DEFAULT '0'
+);
+
+
+
+
+
+-- Enlazar el archivo "tablas.sql" al final (def dis)
+#SOURCE tablas.sql;
